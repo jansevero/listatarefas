964dac56d2b37e4acae9c4f423023c17b92c7241
Jan Severo <jansevero@gmail.com>
2025-05-06 19:09:47 -0300

    aula

---
+++ src/components/Main.js
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-duplicates */
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { Component } from 'react';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    tarefas: [
+      'Beber água',
+      'Estudar',
+      'Jogar',
+    ],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+
+    return (
+      <div className="main">
+        <h1>
+          Lista de tarefas
+        </h1>
+
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
